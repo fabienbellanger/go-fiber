828710ff46361e19797402fe22f5f0fa56b02336
Fabien Bellanger <valentil@gmail.com>
2020-09-18 23:23:27 +0200

    Ajout de l'exemple de la doc pour faire le bench

--- routes_test.go
+++ routes_test.go
@@ -7,6 +7,18 @@ import (
 
 func BenchmarkJsonStream(b *testing.B) {
 	for i := 0; i < b.N; i++ {
+		// // http.Request
+		// req := httptest.NewRequest("GET", "http://google.com", nil)
+		// req.Header.Set("X-Custom-Header", "hi")
+
+		// // http.Response
+		// resp, _ := app.Test(req)
+
+		// // Do something with results:
+		// if resp.StatusCode == 200 {
+		// 	body, _ := ioutil.ReadAll(resp.Body)
+		// 	fmt.Println(string(body)) // => Hello, World!
+		// }
 		s := newServer()
 		s.mode = "production" // Ne fonctionne pas car le logger est initialisé lors du newServer()
 
